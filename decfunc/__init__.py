d979aaa7fdfb75086ce14327900998aab90b5f87
suayip <suayip.541@gmail.com>
2022-01-23 16:29:28 +0300

    Refactor wrapper API

--- decfunc/__init__.py
+++ decfunc/__init__.py
@@ -1,53 +1,40 @@
 import functools
+from inspect import signature
 
-
-__version__ = "0.2.1"
+__version__ = "1.0.0"
 
 __all__ = ("wrapper",)
 
 
-class wrapper:
-    __reserved_fields = ("mutate",)
+class wrapper:  # noqa
+    def __new__(cls, *args, **kwargs):
+        func = None
 
-    def __init__(self, func=None, **options):
-        self.__wrapper_options__ = options
-        self.__decfunc__ = func
+        if args and callable(args[0]):
+            func = args[0]
+            args = args[1:]
+
+        self = super().__new__(cls)
+        signature(self.__init__).bind(*args, **kwargs)
+
+        def _wrapper(f):
+            @functools.wraps(f)
+            def inner(*func_args, **func_kwargs):
+                signature(f).bind(*func_args, **func_kwargs)
+                self.__init__(*args, **kwargs)  # noqa
+                return self.mutate(f, *func_args, **func_kwargs)
+
+            return inner
+
+        self.__wrapper__ = _wrapper
 
         if func is not None:
-            self.__set_fields()
-            functools.update_wrapper(self, self.__decfunc__)
+            return self.__wrapper__(func)
 
-    def __call__(self, *func_args, **func_kwargs):
-        if self.__decfunc__ is not None:
-            return self.mutate(self.__decfunc__, *func_args, **func_kwargs)
+        return self
 
-        func = func_args[0]
-        return self.__class__(func, **self.__wrapper_options__)
-
-    def __str__(self):
-        return self.__class__.__name__
-
-    def __set_fields(self):
-        fields = getattr(self.__class__, "__annotations__", {})
-        options = self.__wrapper_options__
-
-        for option in options:
-            if option not in fields:
-                raise ValueError(
-                    "Unexpected keyword argument for '%s': %s" % (self, option)
-                )
-
-        for field in fields:
-            if field in self.__reserved_fields or field.startswith("__"):
-                raise ValueError("Forbidden field name: '%s'" % field)
-
-            if (field not in options) and (not hasattr(self, field)):
-                raise ValueError(
-                    "Missing required keyword argument for '%s': %s"
-                    % (self, field)
-                )
-            elif field in options:
-                setattr(self, field, options[field])
+    def __call__(self, *args):
+        return self.__wrapper__(*args)
 
     def mutate(self, wrapped, *args, **kwargs):
         raise NotImplementedError(
